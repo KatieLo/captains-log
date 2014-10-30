e1407f00b9a2aff6f2115f57dde1e210a601537a
KatieLo <katielouw@katies-air.gateway.sonic.net>
2014-10-30 13:02:21 -0700

    change formatting when post is retrieved form DB. Line breaks now retained

--- site/controller/view_post.php
+++ site/controller/view_post.php
@@ -6,7 +6,7 @@ include("../../model/post.php");
 $id = check_session();
 $name = ucwords(get_name($id));
 $date = $_GET["date"];
-$post = get_past_post($id, $date);
+$post = nl2br(get_past_post($id, $date));
 
 include("../view/view_post_html.php");
 	
